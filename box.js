63f2412d17b83ae08ce2defb2af03d5fb45f2111
vighnesh27 <vighnesh27@gmail.com>
2020-05-19 22:33:55 +0530

    updated api

--- box.js
+++ box.js
@@ -109,7 +109,7 @@
         */
         function initClient() {
             var API_KEY = 'AIzaSyA2-lCvshvEwuepImB6tbDFljI99UQjq9M'; // TODO: Update placeholder with desired API key.
-            var CLIENT_ID = '125357947992-eogerp8muf4ct04v7ejflhq6cv4hrbtr.apps.googleusercontent.com'; // TODO: Update placeholder with desired client ID.
+            var CLIENT_ID = '125357947992-fan7cv24rlg8f2jjmkp405bt1euer2ki.apps.googleusercontent.com'; // TODO: Update placeholder with desired client ID.
 
             // TODO: Authorize using one of the following scopes:
             //   'https://www.googleapis.com/auth/drive'

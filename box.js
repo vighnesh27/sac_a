c17003e1b9d1f24687eee772f0715914d5ad502a
vighnesh27 <vighnesh27@gmail.com>
2020-05-19 23:24:01 +0530

    updated

--- box.js
+++ box.js
@@ -115,7 +115,7 @@
             //   'https://www.googleapis.com/auth/drive'
             //   'https://www.googleapis.com/auth/drive.file'
             //   'https://www.googleapis.com/auth/spreadsheets'
-            var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
+            var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
             gapi.client.init({
                 'apiKey': API_KEY,
